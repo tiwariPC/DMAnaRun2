0e2bdb98c846381047c107b615b4581b45e35f18
Shin-Shan <syu@cern.ch>
2015-07-20 17:13:58 +0200

    add Single Electron and single muon triggers

--- TreeMaker/src/patHltTree.cc
+++ TreeMaker/src/patHltTree.cc
@@ -38,21 +38,14 @@ patHltTree::Fill(const edm::Event& iEvent)
   for (unsigned int i=0; i<trigResults->size(); i++)
     {
       std::string trigName = trigNames.triggerName(i);
-      //const std::pair<int,int> prescales(hltConfig_.prescaleValues(iEvent,iSetup,triggerNames_[i]));
-      size_t foundEle00=trigName.find("HLT_DoubleEle24_22_eta2p1_WPLoose_Gsf");
-      size_t foundEle01=trigName.find("HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_MW"); 
-      size_t foundEle02=trigName.find("HLT_DoubleEle33_CaloIdL_GsfTrkIdVL"); 
-      size_t foundEle03=trigName.find("HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ"); 
-      size_t foundEle04=trigName.find("HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZ");
-      size_t foundEle05=trigName.find("HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL");
-      size_t foundEle06=trigName.find("HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL");
+      // lepton triggers
+      size_t foundDEle33=trigName.find("HLT_DoubleEle33");
+      size_t foundEle105=trigName.find("HLT_Ele105");
+      size_t foundMu17_Mu8=trigName.find("HLT_Mu17_Mu8");
+      size_t foundMu17_TkMu8=trigName.find("HLT_Mu17_TkMu8");
+      size_t foundMu30_TkMu11=trigName.find("HLT_Mu30_TkMu11");
+      size_t foundMu45=trigName.find("HLT_Mu45");
 
-
-
-      size_t foundMuo00=trigName.find("HLT_Mu17_Mu8");
-      size_t foundMuo01=trigName.find("HLT_Mu17_TkMu8");
-      size_t foundMuo02=trigName.find("HLT_Mu30_TkMu11");
-      
       // jet, met triggers
       size_t foundMETBTag                    = trigName.find("HLT_PFMET120_NoiseCleaned_BTagCSV07");
       size_t foundPFMET170                   = trigName.find("HLT_PFMET170_NoiseCleaned");
@@ -62,21 +55,20 @@ patHltTree::Fill(const edm::Event& iEvent)
       size_t foundDiCEntralPFJet70PFMET120   = trigName.find("HLT_DiCentralPFJet70_PFMET120_NoiseCleaned");
       size_t foundPFHT350_PFMET120           = trigName.find("HLT_PFHT350_PFMET120_NoiseCleaned");
       
+
       if(false) std::cout<<" trigName = "<<trigName
 			<<" : "<<trigResults->accept(i)
-			 <<" : "<<triggerPrescales->getPrescaleForIndex(i)
-			<<" : "<<(foundEle00==std::string::npos)
+			<<" : "<<(foundDEle33==std::string::npos)
+			<<" : "<<(foundEle105==std::string::npos)
+			<<" : "<<(foundMu30_TkMu11==std::string::npos)
+			<<" : "<<(foundMu45==std::string::npos)
 			<<std::endl;
-      if ( foundEle00==std::string::npos &&
-	   foundEle01==std::string::npos &&
-	   foundEle02==std::string::npos &&
-           foundEle03==std::string::npos &&
-           foundEle04==std::string::npos &&
-           foundEle05==std::string::npos &&
-           foundEle06==std::string::npos &&
-       	   foundMuo00==std::string::npos && 
-       	   foundMuo01==std::string::npos && 
-       	   foundMuo02==std::string::npos &&
+      if ( foundDEle33==std::string::npos &&
+	   foundEle105==std::string::npos &&
+       	   foundMu17_Mu8==std::string::npos && 
+       	   foundMu17_TkMu8==std::string::npos && 
+       	   foundMu30_TkMu11==std::string::npos &&
+       	   foundMu45==std::string::npos &&
 	   foundMETBTag==std::string::npos &&
 	   foundPFMET170==std::string::npos &&
 	   foundPFMET190MHT90==std::string::npos &&
