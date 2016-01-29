eb8fd91b676bfb41dbd5b442959695e3f1b5be68
Shin-Shan <syu@cern.ch>
2016-01-29 11:41:27 +0100

    add python example files

--- CrabUtilities/crabConfig.py
+++ CrabUtilities/crabConfig.py
@@ -12,26 +12,7 @@ config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 #config.JobType.psetName = 'miniAODTreeMaker_MC_cfg.py'
 config.JobType.psetName = 'MVA-And-PFUnCorrectedMET.py'
-config.JobType.inputFiles = ['effAreaElectrons_cone03_pfNeuHadronsAndPhotons_25ns.txt','effAreasMuons_cone03_Spring15_25ns.txt',
-                             'Summer15_25nsV6_MC_L1FastJet_AK4PFchs.txt',
-                             'Summer15_25nsV6_MC_L2Relative_AK4PFchs.txt',
-                             'Summer15_25nsV6_MC_L3Absolute_AK4PFchs.txt',
-                             'Summer15_25nsV6_MC_Uncertainty_AK4PFchs.txt',
-                             'Summer15_25nsV6_MC_L1FastJet_AK8PFchs.txt',
-                             'Summer15_25nsV6_MC_L2Relative_AK8PFchs.txt',
-                             'Summer15_25nsV6_MC_L3Absolute_AK8PFchs.txt',
-                             'Summer15_25nsV6_MC_Uncertainty_AK8PFchs.txt',
-                             'Summer15_25nsV6_DATA_L1FastJet_AK4PFchs.txt',
-                             'Summer15_25nsV6_DATA_L2Relative_AK4PFchs.txt',
-                             'Summer15_25nsV6_DATA_L3Absolute_AK4PFchs.txt',
-                             'Summer15_25nsV6_DATA_L2L3Residual_AK4PFchs.txt',
-                             'Summer15_25nsV6_DATA_Uncertainty_AK4PFchs.txt',
-                             'Summer15_25nsV6_DATA_L1FastJet_AK8PFchs.txt',
-                             'Summer15_25nsV6_DATA_L2Relative_AK8PFchs.txt',
-                             'Summer15_25nsV6_DATA_L3Absolute_AK8PFchs.txt',
-                             'Summer15_25nsV6_DATA_L2L3Residual_AK8PFchs.txt',
-                             'Summer15_25nsV6_DATA_Uncertainty_AK8PFchs.txt'
-                             ]
+config.JobType.inputFiles = ['effAreaElectrons_cone03_pfNeuHadronsAndPhotons_25ns.txt','effAreasMuons_cone03_Spring15_25ns.txt']
 config.section_("Data")
 #config.Data.inputDataset = '/DYJetsToLL_M-50_HT-100to200_Tune4C_13TeV-madgraph-tauola/Spring14dr-PU20bx25_POSTLS170_V5-v1/AODSIM '
 config.Data.inputDataset = '/DYJetsToLL_M-50_HT-100to200_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
