fe090af3876f179203454e262239cf890c77ecca
Shin-Shan <syu@cern.ch>
2017-01-28 15:45:54 +0100

    update output tier2

--- CrabUtilities/crabConfig_data.py
+++ CrabUtilities/crabConfig_data.py
@@ -52,6 +52,7 @@ config.JobType.allowUndistributedCMSSW=True
 
 #maxtarballsize = 50 
 config.section_("Site")
-config.Site.storageSite = "T3_TW_NCU"
+#config.Site.storageSite = "T3_TW_NCU"
 #config.Site.storageSite = "T2_CH_CERN"
-##config.Site.storageSite = "T2_US_Wisconsin"                                                         #config.Site.storageSite = "T2_TW_NCHC"                                                                
+##config.Site.storageSite = "T2_US_Wisconsin"                                                         
+config.Site.storageSite = "T2_TW_NCHC"                                                                
