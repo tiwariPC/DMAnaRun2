cd610918a90b6d47ed43dfebb3c2399d623d8a58
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2017-10-30 23:10:29 +0530

    minor update

--- TreeMaker/src/jetTree.cc
+++ TreeMaker/src/jetTree.cc
@@ -338,7 +338,7 @@ jetTree::Fill(const edm::Event& iEvent, edm::EventSetup const& iSetup){
 
     jetSSV_.push_back(jet->bDiscriminator("pfSimpleSecondaryVertexHighPurBJetTags"));
     jetCSV_.push_back(jet->bDiscriminator("combinedSecondaryVertexBJetTags"));
-    jetDeepCSV_.push_back(jet->bDiscriminator("TESTdeepFlavourJetTags:probb")+jet->bDiscriminator("TESTdeepFlavourJetTags:probbb"));
+    jetDeepCSV_.push_back((jet->bDiscriminator("TESTdeepFlavourJetTags:probb"))+(jet->bDiscriminator("TESTdeepFlavourJetTags:probbb")));
     jetSSVHE_.push_back(jet->bDiscriminator("pfSimpleSecondaryVertexHighEffBJetTags"));      
     jetCISVV2_.push_back(jet->bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags"));
     jetTCHP_.push_back(jet->bDiscriminator("pfTrackCountingHighPurBJetTags"));
