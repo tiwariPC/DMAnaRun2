63e3770fdc5811ff2eb8200c908c24c68ecbe170
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2017-10-30 15:25:20 +0530

    adding deepCSV

--- TreeMaker/test/RunCongigTest/treeMaker_Summer16_cfg.py
+++ TreeMaker/test/RunCongigTest/treeMaker_Summer16_cfg.py
@@ -320,8 +320,8 @@ updateJetCollection(
         jetCorrections = ('AK4PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'None'),
         btagDiscriminators = ['deepFlavourJetTags:probudsg', 'deepFlavourJetTags:probb', 'deepFlavourJetTags:probc', 'deepFlavourJetTags:probbb', 'deepFlavourJetTags:probcc'], ## to add discriminators
         btagPrefix = 'TEST'
+        outputModules = ['AODEventContent','AODSIMEventContent','FEVTDEBUGEventContent','FEVTDEBUGHLTEventContent','FEVTEventContent','FEVTHLTALLEventContent','FEVTSIMEventContent','RAWAODSIMEventContent','RAWRECODEBUGHLTEventContent','RAWRECOEventContent','RAWRECOSIMHLTEventContent','RECODEBUGEventContent','RECOEventContent','RECOSIMEventContent','RecoJetsAOD','RecoJetsFEVT','RecoJetsRECO']
         )
-process.out.outputCommands.append('keep *_selectedUpdatedPatJets_*_*')
 
 ##########
 
