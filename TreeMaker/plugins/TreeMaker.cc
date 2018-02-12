c83352febd8baf48d125d77d531bb16fa6980088
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2018-02-12 13:04:57 +0530

    genMET

--- TreeMaker/plugins/TreeMaker.cc
+++ TreeMaker/plugins/TreeMaker.cc
@@ -69,7 +69,6 @@ TreeMaker::TreeMaker(const edm::ParameterSet& iConfig)
       patMetTree_->pfMETRawToken  = consumes<reco::PFMETCollection>(iConfig.getParameter<edm::InputTag>("pfMetRaw"));
       patMetTree_->pfMETToken     = consumes<pat::METCollection>(iConfig.getParameter<edm::InputTag>("pfType1Met"));
       patMetTree_->puppimetToken  = consumes<pat::METCollection>(iConfig.getParameter<edm::InputTag>("puppiMET"));
-      patMetTree_->genParticleToken     = consumes<pat::PackedGenParticle>(iConfig.getParameter<edm::InputTag>("genParticles"));
       // patMetTree_->pfMVAMETToken  = consumes<reco::PFMETCollection>(iConfig.getParameter<edm::InputTag>("pfMVAMET"));                                                        
     }
 
